ae1312f3265aad0ed3f1451f1d88d2c65d55ec3f
headbwoi <tochukwujohn24@gmail.com>
2022-06-01 14:06:32 +0100

    finished project config

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      brightRed: "hsl(12, 88%, 59%)",
+      darkBlue: "hsl(228, 39%, 23%)",
+      darkGrayishBlue: "hsl(227, 12%, 61%)",
+      veryDarkBlue: "hsl(233, 12%, 13%)",
+      veryPaleRed: "hsl(13, 100%, 96%)",
+      varyLightGray: "hsl(0, 0%, 98%)",
+    },
+    fontFamily: {
+      vietnam: ["'Be Vietnam Pro', 'sans-serif'"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
