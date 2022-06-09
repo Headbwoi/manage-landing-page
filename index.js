b6e944d51f31f01e8e577e3ac9e253ff9edecf1a
headbwoi <tochukwujohn24@gmail.com>
2022-06-09 17:26:27 +0100

    did the slider without animation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector("#hamburger")
 const mobileMenu = document.querySelector(".mobilemenu")
+const slideCards = document.querySelectorAll(".card")
+
 
 hamburger.addEventListener("click", ()=>{
     mobileMenu.classList.toggle("show")
@@ -12,3 +14,27 @@ hamburger.addEventListener("click", ()=>{
         mobileMenu.classList.remove("no-scroll")
     }
 })
+
+//slider
+
+let slideIndex = 0
+
+const slideTestimonial = () => {
+    
+    if(window.screen.width > 768){
+        return
+    }
+    slideIndex++
+
+    if(slideIndex > slideCards.length - 1){
+        slideIndex = 0
+    }
+    slideCards.forEach((slide) => {
+        slide.classList.add("hidden")
+    })
+    slideCards[slideIndex].classList.remove("hidden")
+
+}
+setInterval(() => slideTestimonial(), 1000);
+
+slideTestimonial()
