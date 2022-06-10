ba9e5703f485dca8c41259bbaddf21a65f01387d
headbwoi <tochukwujohn24@gmail.com>
2022-06-10 23:13:40 +0100

    finished testimonial section

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const hamburger = document.querySelector("#hamburger")
 const mobileMenu = document.querySelector(".mobilemenu")
 const slideCards = document.querySelectorAll(".card")
 
+ const sliderIndicatorParent = document.querySelector("#slideIndicator"),
+    sliderIndicator = sliderIndicatorParent.querySelectorAll("span")
+
+//     one = document.querySelector("#one"),
+//     two = document.querySelector("#two"),
+//     three = document.querySelector("#three"),
+//     four = document.querySelector("#four")
+
+
 
 hamburger.addEventListener("click", ()=>{
     mobileMenu.classList.toggle("show")
@@ -19,6 +28,7 @@ hamburger.addEventListener("click", ()=>{
 
 let slideIndex = 0
 
+
 const slideTestimonial = () => {
     
     if(window.screen.width > 768){
@@ -34,7 +44,23 @@ const slideTestimonial = () => {
     })
     slideCards[slideIndex].classList.remove("hidden")
 
+    sliderIndicator.forEach(slider => {
+        slider.style.backgroundColor = "white"
+    })
+    if (slideIndex === 0) {
+        one.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+    if (slideIndex === 1) {
+        two.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+    if (slideIndex === 2) {
+        three.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+    if (slideIndex === 3) {
+        four.style.backgroundColor = "hsl(12, 88%, 59%)"
+    }
+
 }
-setInterval(() => slideTestimonial(), 1000);
+setInterval(() => slideTestimonial(), 3000);
 
 slideTestimonial()
